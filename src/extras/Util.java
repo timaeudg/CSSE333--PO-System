78869af0b50639997cdb52780047373c6f061f16
moorejm <moorejm@rose-hulman.edu>
2013-01-31 01:37:23 -0500

    isValidEmail checker

---
+++ src/extras/Util.java
@@ -0,0 +1,23 @@
+package extras;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Util {
+	public static boolean isValidEmailAddress(String email) {
+		Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+		Matcher m = p.matcher(email);
+		return  m.matches();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Email checker: ");
+		String email = "";
+		email = "moorejm@rose-hulman.edu";
+		System.out.printf("Testing: %s\t%s\n", email, isValidEmailAddress(email));
+		email = "cricket.007@live.com";
+		System.out.printf("Testing: %s\t%s\n", email, isValidEmailAddress(email));
+		email = "crikket.007@gmail.com";
+		System.out.printf("Testing: %s\t%s\n", email, isValidEmailAddress(email));
+	}
+}
